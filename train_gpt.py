b5dce9fcee17166ca1252b8c523236769e8e10ce
Roee Shenberg <shenberg@gmail.com>
2025-11-07 11:39:22 +0100

    ugly fix for empty sequences10

--- train_gpt.py
+++ train_gpt.py
@@ -1010,9 +1010,6 @@ class Router:
         torch._check(B - empties > 1)
         tail = seqlens[B - empties :]
         seqlens = seqlens[: B - empties]
-        B_actual = B - empties - 1
-        torch._check(B_actual > 0)
-        torch._check_is_size(B_actual)
 
         # 1) Compute starts and lengths
         starts = seqlens[:-1]
@@ -1031,7 +1028,7 @@ class Router:
         # idx = torch.randperm(B, device=device, generator=generator)[:deficit]
         # keep_counts[idx] += 1
 
-        idx = torch.randperm(B_actual, device=device, generator=generator)
+        idx = torch.randperm(keep_counts.shape[0], device=device, generator=generator)
         keep_counts[idx < deficit] += 1
         # 3) Random key for each token
         pos = torch.arange(L, device=device, dtype=dtype)
