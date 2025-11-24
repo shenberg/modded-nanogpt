5e02a4d3f56359717588accb1a2037fc00ecda13
Roee Shenberg <shenberg@gmail.com>
2025-11-24 15:09:02 +0100

    revert, try neutered tread again6

--- train_gpt.py
+++ train_gpt.py
@@ -1019,7 +1019,7 @@ class GPT(nn.Module):
                 ve=ve[i],
                 sa_lambdas=sa_lambdas[i],
                 seqlens=seqlens,
-                bm_size=bm_sizes[i] if enable_tread and bm_sizes[i] and tread_start_layer < i <= tread_end_layer else bm_sizes[i] * 2,
+                bm_size=bm_sizes[i] if not bm_sizes[i] or (enable_tread and tread_start_layer < i <= tread_end_layer) else bm_sizes[i] * 2,
                 cos=self.yarn.cos,
                 sin=self.yarn.sin,
                 attn_scale=self.yarn.attn_scale
