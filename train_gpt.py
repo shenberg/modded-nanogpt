fbb88127ad3bf3feb7799bc92546ddf63ef0a29a
Roee Shenberg <shenberg@gmail.com>
2025-11-25 15:58:49 +0100

    busy-wait for transfers instead of async

--- train_gpt.py
+++ train_gpt.py
@@ -667,8 +667,9 @@ class DistAdam(torch.optim.Optimizer):
 
         self.should_sync = False
 
+        self._param_to_group = {param : group for group in self.param_groups for param in group['params']}
         self._reduce_scatter_hooks = []
-        self._reduce_scatter_futures = []
+        self._reduce_scatter_futures = {}
         self.register_backward_hooks()
 
     def register_backward_hooks(self):
@@ -687,53 +688,62 @@ class DistAdam(torch.optim.Optimizer):
         grad = param.grad
         rank_size = grad.shape[0] // self.world_size
         grad_slice = torch.empty_like(grad[:rank_size])
-        self._reduce_scatter_futures.append((
-            param,
+        self._reduce_scatter_futures[param] = (
             dist.reduce_scatter_tensor(grad_slice, grad, op=dist.ReduceOp.AVG, async_op=True).get_future(),
             grad_slice
-        ))
+        )
 
     @torch.compile
     @torch.no_grad()
     def step(self):
         rank = dist.get_rank()
         all_gather_futures: list[torch.Future] = []
+        to_remove: list = []
 
-        param_to_group = {param : group for group in self.param_groups for param in group['params']}
-        # go by hook call order 
-        for param, fut, g_slice in self._reduce_scatter_futures:
-            group = param_to_group[param]
-            beta1, beta2 = group['betas']
-            eps = group['eps']
-            wd = group['weight_decay']
-            fut.wait()
+        while self._reduce_scatter_futures:
+            for group in reversed(self.param_groups):
+                beta1, beta2 = group['betas']
+                eps = group['eps']
+                wd = group['weight_decay']
+                for param in reversed(group['params']):
+                    if param not in self._reduce_scatter_futures:
+                        continue
 
-            rank_size = param.shape[0] // self.world_size
-            p_slice = param[rank * rank_size:(rank + 1) * rank_size]
-            lr = group['lr'] * getattr(param, "lr_mul", 1.0)
-            state = self.state[param]
+                    fut, g_slice = self._reduce_scatter_futures[param]
+                    if not fut.done():
+                        continue
+                    fut.wait()
 
-            exp_avg = state["exp_avg"]
-            exp_avg_sq = state["exp_avg_sq"]
-            state["step"] += 1
-            t = state["step"]
-            # weight decay
-            if wd != 0:
-                eff_weight_decay = lr * wd * getattr(param, "wd_mul", 1.0)
-                p_slice.mul_(1 - eff_weight_decay)
-            # update running averages
-            exp_avg.mul_(beta1).add_(g_slice, alpha=1 - beta1)
-            exp_avg_sq.mul_(beta2).addcmul_(g_slice, g_slice, value=1 - beta2)
-            # bias corrections
-            bias1 = 1 - beta1 ** t
-            bias2 = 1 - beta2 ** t
-            # compute step
-            denom = exp_avg_sq.sqrt().add_(eps)
-            step_size = lr * (bias2 ** 0.5 / bias1)
-            update = exp_avg.div(denom).mul_(step_size)
-            p_slice.add_(other=update, alpha=-1.0)
+                    rank_size = param.shape[0] // self.world_size
+                    p_slice = param[rank * rank_size:(rank + 1) * rank_size]
+                    lr = group['lr'] * getattr(param, "lr_mul", 1.0)
+                    state = self.state[param]
 
-            all_gather_futures.append(dist.all_gather_into_tensor(param, p_slice, async_op=True).get_future())
+                    exp_avg = state["exp_avg"]
+                    exp_avg_sq = state["exp_avg_sq"]
+                    state["step"] += 1
+                    t = state["step"]
+                    # weight decay
+                    if wd != 0:
+                        eff_weight_decay = lr * wd * getattr(param, "wd_mul", 1.0)
+                        p_slice.mul_(1 - eff_weight_decay)
+                    # update running averages
+                    exp_avg.mul_(beta1).add_(g_slice, alpha=1 - beta1)
+                    exp_avg_sq.mul_(beta2).addcmul_(g_slice, g_slice, value=1 - beta2)
+                    # bias corrections
+                    bias1 = 1 - beta1 ** t
+                    bias2 = 1 - beta2 ** t
+                    # compute step
+                    denom = exp_avg_sq.sqrt().add_(eps)
+                    step_size = lr * (bias2 ** 0.5 / bias1)
+                    update = exp_avg.div(denom).mul_(step_size)
+                    p_slice.add_(other=update, alpha=-1.0)
+
+                    all_gather_futures.append(dist.all_gather_into_tensor(param, p_slice, async_op=True).get_future())
+                    to_remove.append(param)
+            for p in to_remove:
+                del self._reduce_scatter_futures[p]
+            to_remove.clear()
 
         self._reduce_scatter_futures.clear()
         torch.futures.collect_all(all_gather_futures).wait()
