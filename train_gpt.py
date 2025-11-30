9e97c163e9ed1e4a3bd898177ec1249354b8dbba
Roee Shenberg <shenberg@gmail.com>
2025-11-30 21:37:44 +0100

    more lambdas2

--- train_gpt.py
+++ train_gpt.py
@@ -930,11 +930,11 @@ class Block(nn.Module):
         self.mlp = MLP(dim) if layer_idx != 0 else None
 
     def forward(self, x: Tensor, x0: Tensor, lambdas: Tensor, attn_args: AttnArgs):
-        x = lambdas[0] * x + lambdas[1] * x0
         if self.attn is not None:
             x = lambdas[2] * x + self.attn(norm(x), attn_args)
         if self.mlp is not None:
             x = lambdas[3] * x + self.mlp(norm(x))
+        x = lambdas[0] * x + lambdas[1] * x0
         return x
         # x_res = x
         # if self.attn is not None:
