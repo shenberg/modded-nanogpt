2fcb76eeb12e40c21e591879e6378fd3fa6051f1
Roee Shenberg <shenberg@gmail.com>
2025-12-16 14:06:52 +0100

    increase WD to recover what we lost when tuning learning rate

--- train_gpt.py
+++ train_gpt.py
@@ -1411,7 +1411,7 @@ optimizer1 = DistAdam(
     eps=1e-8,
     weight_decay=0.0,
 )
-optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.023, momentum=0.95, beta2=0.95, weight_decay=1.2)
+optimizer2 = NorMuon(hidden_matrix_params + gate_params, lr=0.023, momentum=0.95, beta2=0.95, weight_decay=1.5)
 optimizers = [optimizer1, optimizer2]
 for opt in optimizers:
     for group in opt.param_groups:
